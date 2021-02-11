0c876e2fddab99ee5e6a6cd70f207e14cc8b9dd9
Jason Gibson <jasongibson@ufl.edu>
2021-02-11 14:31:27 -0500

    Initial commit after CMS Cookiecutter creation, version 1.5

---
+++ quantumml/__init__.py
@@ -0,0 +1,14 @@
+"""
+quantumML
+quantumML is 
+"""
+
+# Add imports here
+from .rest import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
---
+++ quantumml/tests/test_quantumml.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the quantumml package.
+"""
+
+# Import package, test suite, and other packages as needed
+import quantumml
+import pytest
+import sys
+
+def test_quantumml_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "quantumml" in sys.modules
